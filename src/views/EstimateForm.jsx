d76e5e007dc985c1e3b8d336c11b26d3cc6db19f
nick wuerz <nawuerz@gmail.com>
2020-07-12 16:34:41 -0500

    added formspree functionality

--- src/views/EstimateForm.jsx
+++ src/views/EstimateForm.jsx
@@ -16,7 +16,7 @@ import {
   FormControlLabel,
   FormGroup,
   FormLabel,
-  Button
+  Button,
 } from "@material-ui/core";
 import SubmitButton from "../components/SubmitButton";
 
@@ -26,11 +26,17 @@ const useStyles = makeStyles(() => ({
   },
   input: {
     background: "white",
+  },
+  button: {
+    border: "none",
+    background: "white"
   }
 }));
 
+//user state
+
 const EstimateForm = (props) => {
-  const [currentUser, setCurrentUser] = useState({
+  const defaultUserState = {
     firstName: "",
     lastName: "",
     email: "",
@@ -41,7 +47,10 @@ const EstimateForm = (props) => {
     hasAdjuster: false,
     isCommercial: false,
     moreInfo: "",
-  });
+  };
+
+
+  const [currentUser, setCurrentUser] = useState(defaultUserState);
 
   const {
     firstName,
@@ -56,6 +65,16 @@ const EstimateForm = (props) => {
     moreInfo,
   } = currentUser;
 
+  //formspree state
+
+  const [state, setState] = useState({
+    status: "",
+  });
+
+  const { status } = state;
+
+  //event hanlders
+
   const handleInput = (event) => {
     const { value, name } = event.target;
     setCurrentUser({ ...currentUser, [name]: value });
@@ -66,6 +85,26 @@ const EstimateForm = (props) => {
     setCurrentUser({ ...currentUser, [name]: checked });
   };
 
+  const submitForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setState({ status: "SUCCESS" });
+      } else {
+        setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(JSON.stringify(currentUser));
+    console.log(moreInfo);
+    setCurrentUser({ currentUser: defaultUserState });
+  };
+
   const { className, ...rest } = props;
 
   const classes = useStyles();
@@ -73,158 +112,171 @@ const EstimateForm = (props) => {
   return (
     <Card {...rest} className={clsx(classes.root, className)}>
       <Container>
-        <FormControl autoComplete="off" noValidate>
-          <CardHeader
-            title="Let us know how we can help by filling out the form below and we will be in touch to schedule your inspection!"
-            align="center"
-          />
-          <Divider />
-          <CardContent>
-            <Grid container spacing={3}>
-              <Grid item md={6} xs={12}>
-                <TextField
-                  fullWidth
-                  label="First name"
-                  margin="dense"
-                  name="firstName"
-                  value={firstName}
-                  onChange={handleInput}
-                  required
-                  variant="outlined"
-                  className={classes.input}
-                />
+        <form
+          onSubmit={submitForm}
+          action="https://formspree.io/meqrpowg"
+          method="POST"
+        >
+          <FormControl autoComplete="off" noValidate>
+            <CardHeader
+              title="Let us know how we can help by filling out the form below and we will be in touch to schedule your inspection!"
+              align="center"
+            />
+            <Divider />
+            <CardContent>
+              <Grid container spacing={3}>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="First name"
+                    margin="dense"
+                    name="firstName"
+                    value={firstName}
+                    onChange={handleInput}
+                    required
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Last name"
+                    margin="dense"
+                    name="lastName"
+                    value={lastName}
+                    onChange={handleInput}
+                    required
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Email Address"
+                    margin="dense"
+                    name="email"
+                    type="email"
+                    value={email}
+                    onChange={handleInput}
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Phone Number"
+                    margin="dense"
+                    name="phone"
+                    value={phone}
+                    onChange={handleInput}
+                    type="number"
+                    required
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item md={8} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Street Address"
+                    margin="dense"
+                    name="address"
+                    value={address}
+                    onChange={handleInput}
+                    required
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item md={4} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Zip Code"
+                    margin="dense"
+                    name="zip"
+                    value={zip}
+                    onChange={handleInput}
+                    required
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item md={12} xs={12}>
+                  <FormLabel component="legend">
+                    Please select all that apply:
+                  </FormLabel>
+                  <FormGroup align="center">
+                    <Grid item md={12} xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            checked={hasInsurance}
+                            onChange={handleChange}
+                            name="hasInsurance"
+                          />
+                        }
+                        label="I will be filing an insurance Claim"
+                      />
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            checked={hasAdjuster}
+                            onChange={handleChange}
+                            name="hasAdjuster"
+                          />
+                        }
+                        label="An adjuster has been to my home"
+                      />
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            checked={isCommercial}
+                            onChange={handleChange}
+                            name="isCommercial"
+                          />
+                        }
+                        label="I need commercial roofing service"
+                      />
+                    </Grid>
+                  </FormGroup>
+                </Grid>
+                <Grid item md={12} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Additional Info"
+                    margin="dense"
+                    name="moreInfo"
+                    value={moreInfo}
+                    onChange={handleInput}
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    className={classes.input}
+                  />
+                </Grid>
               </Grid>
-              <Grid item md={6} xs={12}>
-                <TextField
-                  fullWidth
-                  label="Last name"
-                  margin="dense"
-                  name="lastName"
-                  value={lastName}
-                  onChange={handleInput}
-                  required
-                  variant="outlined"
-                  className={classes.input}
-                />
-              </Grid>
-              <Grid item md={6} xs={12}>
-                <TextField
-                  fullWidth
-                  label="Email Address"
-                  margin="dense"
-                  name="email"
-                  value={email}
-                  onChange={handleInput}
-                  variant="outlined"
-                  className={classes.input}
-                />
-              </Grid>
-              <Grid item md={6} xs={12}>
-                <TextField
-                  fullWidth
-                  label="Phone Number"
-                  margin="dense"
-                  name="phone"
-                  value={phone}
-                  onChange={handleInput}
-                  type="number"
-                  required
-                  variant="outlined"
-                  className={classes.input}
-                />
-              </Grid>
-              <Grid item md={8} xs={12}>
-                <TextField
-                  fullWidth
-                  label="Street Address"
-                  margin="dense"
-                  name="address"
-                  value={address}
-                  onChange={handleInput}
-                  required
-                  variant="outlined"
-                  className={classes.input}
-                />
-              </Grid>
-              <Grid item md={4} xs={12}>
-                <TextField
-                  fullWidth
-                  label="Zip Code"
-                  margin="dense"
-                  name="zip"
-                  value={zip}
-                  onChange={handleInput}
-                  required
-                  variant="outlined"
-                  className={classes.input}
-                />
-              </Grid>
-              <Grid item md={12} xs={12}>
-                <FormLabel component="legend">
-                  Please select all that apply:
-                </FormLabel>
-                <FormGroup align="center">
-                  <Grid item md={12} xs={12}>
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={hasInsurance}
-                          onChange={handleChange}
-                          name="hasInsurance"
-                        />
-                      }
-                      label="I will be filing an insurance Claim"
-                    />
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={hasAdjuster}
-                          onChange={handleChange}
-                          name="hasAdjuster"
-                        />
-                      }
-                      label="An adjuster has been to my home"
-                    />
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={isCommercial}
-                          onChange={handleChange}
-                          name="isCommercial"
-                        />
-                      }
-                      label="I need commercial roofing service"
-                    />
-                  </Grid>
-                </FormGroup>
-              </Grid>
-              <Grid item md={12} xs={12}>
-                <TextField
-                  fullWidth
-                  label="Additional Info"
-                  margin="dense"
-                  name="moreInfo"
-                  value={moreInfo}
-                  onChange={handleInput}
-                  multiline
-                  rows={4}
-                  variant="outlined"
-                  className={classes.input}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-          <Divider />
-          <CardActions classes={{root: classes.root}} style={{ textAlign: "center" }}>
-            {firstName && lastName && email && phone && address && zip !== "" ? (
-              <SubmitButton />
+            </CardContent>
+            <Divider />
+            <CardActions
+              classes={{ root: classes.root }}
+              style={{ textAlign: "center" }}
+            >
+              {/* {status === "SUCCESS" ? <p>Thanks!</p> : <button style={{border: "none", background:"white"}}><SubmitButton type="submit"/></button>}
+              {status === "ERROR" && <p>Ooops! There was an error.</p>} */}
+
+              {firstName && lastName && email && phone && address && zip !== "" & status !== "SUCCESS" ? (
+              <button className={classes.button}><SubmitButton /></button>
             ):(
               <Button disabled align="center" style={{ backgroundColor: "grey", color: "white" }}>
                 Submit
               </Button>
             )}
-          </CardActions>
-        </FormControl>
+            </CardActions>
+          </FormControl>
+        </form>
       </Container>
     </Card>
   );
