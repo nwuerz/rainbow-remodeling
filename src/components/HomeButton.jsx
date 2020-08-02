e80a6b505e5bc58a0d9cbcfde431121225abeb77
nick wuerz <nawuerz@gmail.com>
2020-08-01 20:05:23 -0500

    updated home button

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Fab from "@material-ui/core/Fab";
 import HomeIcon from "@material-ui/icons/Home";
 import { useHistory } from "react-router-dom";
@@ -6,16 +6,26 @@ import { useHistory } from "react-router-dom";
 const HomeButton = () => {
   let history = useHistory();
 
+  const useInitialFocus = (ref, title) => {
+    useEffect(() => {
+      ref.current.focus();
+      document.title = title;
+    }, [ref, title]);
+  };
+
+  const mainRef = useRef(null);
+  useInitialFocus(mainRef, "Home Button");
+
   return (
     <>
       <Fab
-        autofocus
         style={{
           position: "absolute",
           right: "0px",
           backgroundColor: "transparent",
         }}
         onClick={() => history.push("/")}
+        ref={mainRef}
       >
         <HomeIcon />
       </Fab>
