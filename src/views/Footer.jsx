ec68f500e7b88eb4795565a847c6660274cda84d
nick wuerz <nawuerz@gmail.com>
2020-08-01 19:08:49 -0500

    updated footer

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -33,24 +33,24 @@ const Footer = (props) => {
 
   return (
     <Grid className={classes.root} container>
-      <Grid item align="center" md={12}>
+      <Grid item align="center" xs={12} s={12} md={12}>
         <Copyright />
       </Grid>
-      <Grid item align="center" md={12}>
+      <Grid item align="center" xs={12} s={12} md={12}>
         {"2817 Meadow Park"}
       </Grid>
-      <Grid item align="center" md={12}>
+      <Grid item align="center" xs={12} s={12} md={12}>
         {"Garland, TX 75040"}
       </Grid>
-      <Grid item align="center" md={12}>
+      <Grid item align="center" xs={12} s={12} md={12}>
         <a className={classes.phone} href="tel:469-855-6912">
           469.855.6912
         </a>
       </Grid>
-      <Grid item align="center" md={12}>
+      <Grid item align="center" xs={12} s={12} md={12}>
         {"rainbowremodeling@yahoo.com"}
       </Grid>
-      <Grid className={classes.link} item align="center" md={12}>
+      <Grid className={classes.link} item align="center" xs={12} s={12} md={12}>
         {"Site by "}
         <Link
           color="secondary"
