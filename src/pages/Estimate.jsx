67d6440f10497388bf57c9bb701c50150429d65b
nick <nickwuerz@gmail.com>
2024-08-24 14:14:42 -0500

    updates import statement

--- src/pages/Estimate.jsx
+++ src/pages/Estimate.jsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import React from "react";
 import EstimateHeader from "../views/EstimateHeader";
 import EstimateForm from "../views/EstimateForm";
 import HomeButton from "../components/HomeButton";
